4711c9e1272bb1b1a9ebd43a16cbaa823f74699b
routersys <github.routersys@gmail.com>
2026-09-13 10:22:13 +0900

    プロキシの寸法を求める計算を追加する

---
+++ ProxyForge/Encoding/ProxyGeometry.cs
@@ -0,0 +1,37 @@
+namespace ProxyForge.Encoding;
+
+internal readonly record struct ProxyGeometry(int Width, int Height);
+
+internal static class ProxyGeometryCalculator
+{
+    public const int MinimumDimension = 2;
+    public const int MaximumDimension = 16384;
+    public const int MinimumScale = 10;
+    public const int MaximumScale = 100;
+
+    public static int AlignEven(int value)
+    {
+        var aligned = value & ~1;
+        return aligned < MinimumDimension ? MinimumDimension : aligned;
+    }
+
+    public static ProxyGeometry Calculate(int displayWidth, int displayHeight, int scale)
+    {
+        ArgumentOutOfRangeException.ThrowIfLessThan(displayWidth, 1);
+        ArgumentOutOfRangeException.ThrowIfLessThan(displayHeight, 1);
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(displayWidth, MaximumDimension);
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(displayHeight, MaximumDimension);
+
+        var clamped = Math.Clamp(scale, MinimumScale, MaximumScale);
+        return new ProxyGeometry(
+            ScaleDimension(displayWidth, clamped),
+            ScaleDimension(displayHeight, clamped));
+    }
+
+    static int ScaleDimension(int dimension, int scale)
+    {
+        var scaled = AlignEven((int)Math.Round(dimension * (double)scale / MaximumScale, MidpointRounding.AwayFromZero));
+        var limit = AlignEven(dimension);
+        return scaled > limit ? limit : scaled;
+    }
+}
